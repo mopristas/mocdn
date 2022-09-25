6eea94f2bb9ba71728fe43d0ba2ec814e750ff7f
mopristas <maros.pristas@gmail.com>
2022-09-25 17:33:42 +0200

    Update yachtsup.js

--- yachtsup.js
+++ yachtsup.js
@@ -1,5 +1,30 @@
-jQuery(document).ready(function () { //docready
-  //conversion functions
+<script>console.log("zaraz supportx1")</script> 
+<noscript> <a href='https://www.clickcease.com' rel='nofollow'><img src='https://monitor.clickcease.com/stats/stats.aspx' alt='ClickCease'/></a> </noscript>
+<script src="https://kit.fontawesome.com/b051e1b394.js" crossorigin="anonymous"></script>
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+        var _smartsupp = _smartsupp || {};
+        _smartsupp.key = 'eb671a373983bd4e8f30b487fd610e6c6d19b07e';
+        window.smartsupp || (function (d) {
+            var s, c, o = smartsupp = function () {
+                o._.push(arguments)
+            };
+            o._ = [];
+            s = d.getElementsByTagName('script')[0];
+            c = d.createElement('script');
+            c.type = 'text/javascript';
+            c.charset = 'utf-8';
+            c.async = true;
+            c.src = 'https://www.smartsuppchat.com/loader.js?';
+            s.parentNode.insertBefore(c, s);
+        })(document);
+        smartsupp('chat:close');
+        smartsupp('on', 'message_received', function (message) {
+            smartsupp('chat:open');
+        });   
+        </script>
+<script>
+     //conversion functions
 function MoextractEmails(text) {
     //return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi);
     return text.match(/[a-z0-9_\-\+]+@[a-z0-9\-]+\.([a-z]{2,3})(?:\.[a-z]{2})?/i);
@@ -68,4 +93,4 @@ smartsupp('on', 'message_received', function (message) {
         }
         dataLayer.push({'event': 'contact_5_submitted'});
     });
-}); //docready
+    </script>
